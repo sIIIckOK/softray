e4dc7a9a5c2b357b409134b6ba224315913d1c0d
siiickok <siddharthtdd123@gmail.com>
2026-01-26 04:54:17 +0900

    Fix windows build for raylib

--- vendor_build_windows.bat
+++ vendor_build_windows.bat
@@ -1,6 +1,13 @@
 @echo off
 
-make -C .\vendor\raylib\src\
+SET CC=gcc
+SET CFLAGS=-DPLATFORM_DESKTOP -DGRAPHICS_API_OPENGL_33
+SET LINK=-I.\external\glfw\include
+
+PUSHD .\vendor\raylib\src\
+%CC% %CFLAGS% -c *.c %LINK%
+ar rcs libraylib.a *.o
+POPD
 
 MOVE .\vendor\raylib\src\libraylib.a .\lib\windows
 
