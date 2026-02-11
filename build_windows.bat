e68dee0c6c52bdf5b6d4cfb38a2338ed4a45bdf0
siiickok <siddharthtdd123@gmail.com>
2026-02-11 22:06:13 +0900

    windows bug fix

--- build_windows.bat
+++ build_windows.bat
@@ -1,5 +1,5 @@
-if not exist ".\lib\windows" (
-    mkdir ".\lib\windows"
+if not exist ".\bin" (
+    mkdir ".\bin"
 )
 
 gcc -o3 ./platform/plat_raylib.c -o ./bin/plat_raylib_windows -I./include/ -L./lib/windows/ -lraylib -lm -lgdi32 -lwinmm -lopengl32 -limm32 -lole32 -luuid -lcomdlg32 -lshell32
